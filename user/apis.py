dccaf22e8ff615966fa4c406ba864ef6660fddc0
jodyboy <elite.crew66@gmail.com>
2023-11-24 23:31:08 +0700

    Authorization works perfectly

--- user/apis.py
+++ user/apis.py
@@ -1,19 +1,26 @@
-from flask import Blueprint, request
-import jwt, os
+from flask import Blueprint, request, jsonify
+# import jwt, os
 from user.models import User
+from auth.utils import decode_jwt
 
 user_blueprint = Blueprint("user",__name__)
 
 @user_blueprint.route("/", methods=["GET"])
 def get_user():
-    token = request.headers.get('Authorization')
-    payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithm="HS256")
+    payload = decode_jwt(request.headers.get('Authorization'))
+    if not payload:
+        return {"error": "Token is not valid!"}, 401
+    
+    user = User.query.get(payload["user_id"])
 
-    user_id = payload["user_id"]
-    user = User.query.get(user_id)
+    # token = request.headers.get('Authorization')
+    # payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms="HS256")
+
+    # user_id = payload["user_id"]
+    # user = User.query.get(user_id)
 
     if not user:
-        return {"message": "User not found"}, 404
+        return {"error": "User not found"}, 404
     
     return {
         'id': user.id,
